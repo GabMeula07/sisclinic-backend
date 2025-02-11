9bc5f4e6203d6386f86d20716ebf94e97c580a63
GabMeula07 <gabrielqueirozmeula@gmail.com>
2025-02-11 00:47:46 -0300

    fix: change cross origin

--- app/main.py
+++ app/main.py
@@ -17,12 +17,15 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Você pode definir ["http://localhost:3000"] para maior segurança
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+@app.get("/")
+def hello_world():
+    return {"msg": "Hello world"}
 
 
 app.include_router(admin_routes, tags=["Admin"])
